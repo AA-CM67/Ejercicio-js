5b1a8436caccbc8f97b59c6e71154c60e0f95772
AA-CM67 <you@example.com>
2026-03-06 13:22:56 -0500

    feat: ejercicio 7

--- src/ejercicios.js
+++ src/ejercicios.js
@@ -147,7 +147,7 @@ function redondearHaciaArriba(num) {return Math.ceil (num);
 }
 
 // Test 24
-function numeroRandom() {return Math.
+function numeroRandom() {return Math.Random ();
 	//Generar un número al azar entre 0 y 1 y devolverlo
 	//Pista: investigá qué hace el método Math.random()
 	// Tu código:
